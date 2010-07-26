12da9d5d6d575d3da892972c053dc71d276a6b59
Martin DeMello <martindemello@gmail.com>
2010-07-26 20:16:40 +0530

    don't need to distinguish null and atom in reverse

--- 2.27.scm
+++ 2.27.scm
@@ -1,11 +1,9 @@
 #lang scheme
 
 (define (deep-reverse x)
-  (define (atom? x) (not (pair? x)))
-  (cond
-    ((null? x) '())
-    ((atom? x) x)
-    (else (map deep-reverse (reverse x)))))
+  (if (pair? x)
+      (map deep-reverse (reverse x))
+      x))
 
 (define x (list (list 1 2) (list 3 4)))
 

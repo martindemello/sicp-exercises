352234d70b2a683480348801e4959c06b6df5788
Martin DeMello <martindemello@gmail.com>
2010-07-26 20:18:51 +0530

    don't use nil

--- 2.21.scm
+++ 2.21.scm
@@ -9,8 +9,14 @@
 
 (define (square-list items)
   (if (null? items)
-      nil
+      '()
       (cons (* (car items) (car items)) (square-list (cdr items)))))
 
-(define (square-list items)
+(define (square-list-map items)
   (map (lambda (x) (* x x)) items))
+
+(define xs '(1 2 3 4 5))
+
+(display (square-list xs))
+(newline)
+(display (square-list-map xs))

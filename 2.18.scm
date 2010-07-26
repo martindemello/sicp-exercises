27a50168954fc9577b66840fe6500279b2b3ee12
Martin DeMello <martindemello@gmail.com>
2010-07-26 20:10:23 +0530

    split out 2.17

--- 2.18.scm
+++ 2.18.scm
@@ -1,12 +1,4 @@
 #lang scheme
-;Exercise 2.17. Define a procedure last-pair that returns the list that contains only the last element of a
-;given (nonempty) list:
-;(last-pair (list 23 72 149 34))
-;(34)
-
-(define (last-pair l)
-  (if (null? (cdr l)) l (last-pair (cdr l))))
-
 ;Exercise 2.18. Define a procedure reverse that takes a list as argument and returns a list of the same
 ;elements in reverse order:
 ;(reverse (list 1 4 9 16 25))
